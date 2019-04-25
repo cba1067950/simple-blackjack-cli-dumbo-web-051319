f86dbfbaed95c5908540853010fa63505018589a
Joe Jamsky <cba1067950@yahoo.com>
2019-04-25 00:14:14 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -39,15 +39,16 @@ def hit?(current_total)
   # code hit? here
   prompt_user
   input = get_user_input
-  if input == 's'
-      display_card_total
-    elsif input == 'h'
-      current_total += deal_card
-      display_card_total
-    else
-      invalid_command
-      prompt_user
-  end
+  
+  #if input == 's'
+  #    display_card_total
+  #  elsif input == 'h'
+  #    current_total += deal_card
+  #    display_card_total
+  #  else
+  #    invalid_command
+  #    prompt_user
+  #end
 end
 
 def invalid_command
